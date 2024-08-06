9167a36fa81c6083a700b044b7095e66ada28188
Pedro Goiânia <pedro.paulods@hotmail.com>
2024-08-06 13:55:59 -0300

    feat(config): added configurations to project

---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+"use client";
+import Link from "next/link";
+import ThemeChanger from "./DarkSwitch";
+import Image from "next/image";
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react";
+import { useTheme } from "next-themes";
+import { useEffect } from "react";
+
+export const Navbar = () => {
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    console.log("theme", theme);
+  }, [theme])
+
+  return (
+    <div className="w-full">
+      <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-0">
+        {/* Logo  */}
+        <Disclosure>
+          {({ open }) => (
+            <>
+              <div className="flex flex-wrap items-center justify-between w-full lg:w-auto">
+                <Link href="/">
+                  <span className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
+                    <span>
+                      <Image
+                        src={"/images/logo-join-white.png"}
+                        alt="Logo Join"
+                        width="150"
+                        height="80"
+                        className="w-30"
+                      />
+                    </span>
+                    {/* <span>Nextly</span> */}
+                  </span>
+                </Link>
+              </div>
+            </>
+          )}
+        </Disclosure>
+      </nav>
+    </div>
+  );
+};
