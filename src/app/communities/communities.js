d4f30ea589adecdb3ef70c6a7a78f5fbd957b1ff
GleidsonDaniel <gleidson10daniel@hotmail.com>
2025-07-01 16:15:52 -0300

    ordem

--- src/app/communities/communities.js
+++ src/app/communities/communities.js
@@ -1,11 +1,124 @@
 export const communities = [
   {
+    name: "Anapolivre",
+    description:
+      "É uma comunidade de articulação de grupos e pessoas interessadas em colaborar com o conhecimento livre em Anápolis. O ativismo pela liberdade de conhecimento envolve ações voltadas para Software Livre e Cultura Livre. Incentivamos o compartilhamento do conhecimento por meio de licenças livres como GPL e Creative Commons (cc-by-sa), assim, tanto a troca de informações tecnológicas quanto culturais são bem vindas.",
+    logo: "/images/communities/10.png",
+    category: "Tecnologia",
+    socialLinks: [{ type: "telegram", url: "https://t.me/+P2LGWtuAXOyYIP5Z" }],
+  },
+  {
+    name: "AWS UG Anápolis",
+    description:
+      "Comunidade dedicada a compartilhar tudo sobre a nuvem AWS para o público Anapolino, em todos os níveis de experiência! Se você é apaixonado por Cloud Computing e adora discutir tecnologia, venha fazer parte!",
+    logo: "/images/communities/7.png",
+    category: "Tecnologia",
+    socialLinks: [
+      { type: "youtube", url: "https://www.youtube.com/@AWSUserGroupAnapolis" },
+    ],
+  },
+  {
+    name: "AWS User Group Goiânia",
+    description:
+      "Grupo de usuários AWS em Goiânia, com foco em compartilhar conhecimento e fazer networking.",
+    logo: "/images/communities/6.png",
+    category: "Tecnologia",
+    socialLinks: [
+      {
+        type: "whatsapp",
+        url: "https://chat.whatsapp.com/Cm4A0q3bC7qJJMdfsrhbmz",
+      },
+      { type: "instagram", url: "https://www.instagram.com/awsgoiania" },
+      { type: "youtube", url: "https://www.youtube.com/@AWSGO" },
+      { type: "meetup", url: "https://www.meetup.com/aws-go" },
+      { type: "linkedin", url: "https://www.linkedin.com/company/aws-go" },
+    ],
+  },
+  {
+    name: "Cloud Native Goiânia",
+    description:
+      "Cloud Native Goiânia é uma comunidade que busca promover o uso de ferramentas e tecnologias de código livre e nativas para nuvem (https://landscape.cncf.io/) e ajudar as pessoas que desejam aprender ou aperfeiçoar seus conhecimentos nessas ferramentas.",
+    logo: "/images/communities/5.png",
+    category: "Tecnologia",
+    socialLinks: [
+      {
+        type: "whatsapp",
+        url: "https://chat.whatsapp.com/IfI8uspJnJxGE2HDlE6Wt5",
+      },
+      {
+        type: "instagram",
+        url: "https://www.instagram.com/cloudnativegoiania",
+      },
+      {
+        type: "linkedin",
+        url: "https://www.linkedin.com/company/cloudnativegoiania",
+      },
+      { type: "youtube", url: "https://www.youtube.com/@cloudnativegoiania" },
+    ],
+  },
+  {
+    name: "GOJava",
+    description:
+      'Fundada em 12/09/2001, a comunidade é o Grupo de Usuários Java (JUG) de Goiás e tem como missão gerar valor para os desenvolvedores e empresas goianas, capacitando e catalisando relacionamentos sustentáveis entre o mercado, estudantes, profissionais e "java vendors".',
+    logo: "/images/communities/11.png",
+    category: "Tecnologia",
+    socialLinks: [
+      { type: "website", url: "https://comunidades.tech/c/gojava" },
+    ],
+  },
+  {
+    name: "GUOCB - Grupo de usuários Oracle Cloud do Brasil",
+    description:
+      "Focado em usuários de nuvem Oracle e suas tecnologias relacionadas, troca e compartilhamento de informações além de participação em eventos.",
+    logo: "/images/communities/1.png",
+    category: "Tecnologia",
+    socialLinks: [{ type: "telegram", url: "https://t.me/GUOCB" }],
+  },
+  {
     name: "GYNSec",
     description:
       "A GynSec é uma comunidade de cibersegurança que nasceu no coração do Brasil, em Goiânia-GO! Desde nossa fundação, respiramos tecnologia e, principalmente, security tech. Nosso propósito é claro: disseminar conhecimento, conectar entusiastas e profissionais, e fortalecer o ecossistema de segurança digital.",
     logo: "/images/communities/3.png",
     category: "Tecnologia",
     socialLinks: [{ type: "telegram", url: "https://t.me/gynsec" }],
+  },
+  {
+    name: "Magnatas",
+    description:
+      "A Magnatas é uma comunidade de empreendedores, focada em gerar conexão nossas empresas e os outros empreendedores do grupo. Abraçamos também iniciativas de startups, promovendo espaço para validação, assessorias e palco para visibilidade. Além disso, incentivamos pessoas não empreendedoras a conhecer mais do meio e se capacitar. Parte do nosso propósito é fazer a diferença na comunidade goiana, oferecendo tecnologia de qualidade, e um bom ambiente de networking.",
+    logo: "/images/communities/9.png",
+    category: "Empreendedorismo",
+    socialLinks: [
+      {
+        type: "whatsapp",
+        url: "https://chat.whatsapp.com/FBmEEa1CNq94DWoR5UBTwH",
+      },
+      { type: "instagram", url: "https://www.instagram.com/magnatas.hub" },
+      {
+        type: "linkedin",
+        url: "https://www.linkedin.com/company/magnatas-hub",
+      },
+    ],
+  },
+  {
+    name: "Override Community",
+    description:
+      "Na Override Community, acreditamos no poder da colaboração. Somos o ponto de encontro de profissionais de TI em Goiás que buscam aprendizado contínuo e troca de experiências.",
+    logo: "/images/communities/4.png",
+    category: "Tecnologia",
+    socialLinks: [
+      {
+        type: "whatsapp",
+        url: "https://chat.whatsapp.com/J59z0REpbOf0KLDd2GiTX8",
+      },
+      { type: "instagram", url: "https://www.instagram.com/overridenet" },
+      { type: "youtube", url: "https://www.youtube.com/@OverrideCommunityNet" },
+      { type: "meetup", url: "https://www.meetup.com/pt-BR/override" },
+      {
+        type: "linkedin",
+        url: "https://www.linkedin.com/company/override-community",
+      },
+    ],
   },
   {
     name: "PyLadies Goiânia",
@@ -45,109 +158,6 @@ export const communities = [
     ],
   },
   {
-    name: "Override Community",
-    description:
-      "Na Override Community, acreditamos no poder da colaboração. Somos o ponto de encontro de profissionais de TI em Goiás que buscam aprendizado contínuo e troca de experiências.",
-    logo: "/images/communities/4.png",
-    category: "Tecnologia",
-    socialLinks: [
-      {
-        type: "whatsapp",
-        url: "https://chat.whatsapp.com/J59z0REpbOf0KLDd2GiTX8",
-      },
-      { type: "instagram", url: "https://www.instagram.com/overridenet" },
-      { type: "youtube", url: "https://www.youtube.com/@OverrideCommunityNet" },
-      { type: "meetup", url: "https://www.meetup.com/pt-BR/override" },
-      {
-        type: "linkedin",
-        url: "https://www.linkedin.com/company/override-community",
-      },
-    ],
-  },
-  {
-    name: "Anapolivre",
-    description:
-      "É uma comunidade de articulação de grupos e pessoas interessadas em colaborar com o conhecimento livre em Anápolis. O ativismo pela liberdade de conhecimento envolve ações voltadas para Software Livre e Cultura Livre. Incentivamos o compartilhamento do conhecimento por meio de licenças livres como GPL e Creative Commons (cc-by-sa), assim, tanto a troca de informações tecnológicas quanto culturais são bem vindas.",
-    logo: "/images/communities/10.png",
-    category: "Tecnologia",
-    socialLinks: [{ type: "telegram", url: "https://t.me/+P2LGWtuAXOyYIP5Z" }],
-  },
-  {
-    name: "AWS User Group Goiânia",
-    description:
-      "Grupo de usuários AWS em Goiânia, com foco em compartilhar conhecimento e fazer networking.",
-    logo: "/images/communities/6.png",
-    category: "Tecnologia",
-    socialLinks: [
-      {
-        type: "whatsapp",
-        url: "https://chat.whatsapp.com/Cm4A0q3bC7qJJMdfsrhbmz",
-      },
-      { type: "instagram", url: "https://www.instagram.com/awsgoiania" },
-      { type: "youtube", url: "https://www.youtube.com/@AWSGO" },
-      { type: "meetup", url: "https://www.meetup.com/aws-go" },
-      { type: "linkedin", url: "https://www.linkedin.com/company/aws-go" },
-    ],
-  },
-  {
-    name: "Magnatas",
-    description:
-      "A Magnatas é uma comunidade de empreendedores, focada em gerar conexão nossas empresas e os outros empreendedores do grupo. Abraçamos também iniciativas de startups, promovendo espaço para validação, assessorias e palco para visibilidade. Além disso, incentivamos pessoas não empreendedoras a conhecer mais do meio e se capacitar. Parte do nosso propósito é fazer a diferença na comunidade goiana, oferecendo tecnologia de qualidade, e um bom ambiente de networking.",
-    logo: "/images/communities/9.png",
-    category: "Empreendedorismo",
-    socialLinks: [
-      {
-        type: "whatsapp",
-        url: "https://chat.whatsapp.com/FBmEEa1CNq94DWoR5UBTwH",
-      },
-      { type: "instagram", url: "https://www.instagram.com/magnatas.hub" },
-      {
-        type: "linkedin",
-        url: "https://www.linkedin.com/company/magnatas-hub",
-      },
-    ],
-  },
-  {
-    name: "Cloud Native Goiânia",
-    description:
-      "Cloud Native Goiânia é uma comunidade que busca promover o uso de ferramentas e tecnologias de código livre e nativas para nuvem (https://landscape.cncf.io/) e ajudar as pessoas que desejam aprender ou aperfeiçoar seus conhecimentos nessas ferramentas.",
-    logo: "/images/communities/5.png",
-    category: "Tecnologia",
-    socialLinks: [
-      {
-        type: "whatsapp",
-        url: "https://chat.whatsapp.com/IfI8uspJnJxGE2HDlE6Wt5",
-      },
-      {
-        type: "instagram",
-        url: "https://www.instagram.com/cloudnativegoiania",
-      },
-      {
-        type: "linkedin",
-        url: "https://www.linkedin.com/company/cloudnativegoiania",
-      },
-      { type: "youtube", url: "https://www.youtube.com/@cloudnativegoiania" },
-    ],
-  },
-  {
-    name: "GUOCB - Grupo de usuários Oracle Cloud do Brasil",
-    description:
-      "Focado em usuários de nuvem Oracle e suas tecnologias relacionadas, troca e compartilhamento de informações além de participação em eventos.",
-    logo: "/images/communities/1.png",
-    category: "Tecnologia",
-    socialLinks: [{ type: "telegram", url: "https://t.me/GUOCB" }],
-  },
-  {
-    name: "GOJava",
-    description:
-      'Fundada em 12/09/2001, a comunidade é o Grupo de Usuários Java (JUG) de Goiás e tem como missão gerar valor para os desenvolvedores e empresas goianas, capacitando e catalisando relacionamentos sustentáveis entre o mercado, estudantes, profissionais e "java vendors".',
-    logo: "/images/communities/11.png",
-    category: "Tecnologia",
-    socialLinks: [
-      { type: "website", url: "https://comunidades.tech/c/gojava" },
-    ],
-  },
-  {
     name: "StartupGO",
     description:
       "A comunidade StartupGO tem como objetivo tornar Goiás um estado mais inovador, promovendo a cultura empreendedora e apoiando fundadores de startups. Atua em todo o estado, com base principal em Goiânia e região metropolitana, e no apoio a outras regiões como Rio Verde, Jataí, Catalão e Caldas Novas. Suas principais atividades envolvem a organização e apoio à eventos presenciais e online como hackathons, meetups e o Techstars Startup Weekend.",
@@ -155,16 +165,6 @@ export const communities = [
     category: "Empreendedorismo",
     socialLinks: [
       { type: "instagram", url: "https://instagram.com/startup.goias" },
-    ],
-  },
-  {
-    name: "AWS UG Anápolis",
-    description:
-      "Comunidade dedicada a compartilhar tudo sobre a nuvem AWS para o público Anapolino, em todos os níveis de experiência! Se você é apaixonado por Cloud Computing e adora discutir tecnologia, venha fazer parte!",
-    logo: "/images/communities/7.png",
-    category: "Tecnologia",
-    socialLinks: [
-      { type: "youtube", url: "https://www.youtube.com/@AWSUserGroupAnapolis" },
     ],
   },
 ];
