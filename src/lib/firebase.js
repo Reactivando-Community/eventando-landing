1ee294177325969006af5be1404a4097c984f642
Pedro Goiania <pedro.paulods@hotmail.com>
2026-03-26 02:09:31 -0300

    removed firebase from hardcoded

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp, getApps } from "firebase/app";
 import { getAnalytics, logEvent as firebaseLogEvent, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyByNfZ2iK0wUVfxFL5HIQ8gn4kfPM7t_Uw",
-  authDomain: "swanapolis.firebaseapp.com",
-  projectId: "swanapolis",
-  storageBucket: "swanapolis.firebasestorage.app",
-  messagingSenderId: "26129862584",
-  appId: "1:26129862584:web:a49aae307afbeff778094d",
-  measurementId: "G-HS9N4C07LL",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase (singleton)
